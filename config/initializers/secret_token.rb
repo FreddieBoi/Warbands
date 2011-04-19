042a788f4a11b4a279355d0082702e4d7530798d
Freddie <freddieboi@gmail.com>
2011-04-19 14:55:30 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Warbands::Application.config.secret_token = '60f22fe861ef6ac714f2db961a53c6eb99ab67446dccd9e9f6621eab3268c40df56358c0545676051d87b8753875f92587a311f5d679556b58d2f9b3caceb5ab'
