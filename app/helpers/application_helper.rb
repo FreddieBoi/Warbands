17dfc1fea3bb19605659bc2e6b6e8e36e698a3aa
Freddie <freddieboi@gmail.com>
2011-04-25 12:33:21 +0200

    Added title helpers and some layout partials to improve structure and readability

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  # Get the title of the app
+  def app_title
+    "Warbands Online"
+  end
+
+  # Get the title of the page
+  def page_title
+    if @title.nil?
+      app_title
+    else
+      "#{app_title} > #{@title}"
+    end
+  end
 end
