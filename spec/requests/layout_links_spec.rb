8ea5dee85f594cca61acfe7f917c08f9a46d5c18
Freddie <freddieboi@gmail.com>
2011-04-26 01:44:14 +0200

    Added click_link tests

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "LayoutLinks" do
 
-  it "should have a Home page at '/'" do
+  it "should have a Home (root) page at '/'" do
     get '/'
     response.should have_selector('title', :content => "Warbands Online")
   end
@@ -20,5 +20,17 @@ describe "LayoutLinks" do
   it "should have a Help page at '/help'" do
     get '/help'
     response.should have_selector('title', :content => "Help")
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    response.should have_selector('title', :content => "About")
+    click_link "Help"
+    response.should have_selector('title', :content => "Help")
+    click_link "Contact"
+    response.should have_selector('title', :content => "Contact")
+    click_link "Home"
+    response.should have_selector('title', :content => "Warbands Online")
   end
 end
