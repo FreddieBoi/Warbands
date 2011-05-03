d41b4aadeba5f60b0bf5202fe0f83a1e4f10245a
Freddie <freddieboi@gmail.com>
2011-05-03 02:55:42 +0200

    Show proper 404 page on non-standard format requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   # Allow html, js, json, xml, rss. Render 404 if not allowed.
   def allow_only_standard_requests
     if params[:format] && (["html", "js", "json", "xml", "rss"].exclude?(params[:format]))
-      render :file => "#{RAILS_ROOT}/public/404.html"
+      raise ActionController::RoutingError.new('No route matches "'+request.url+'"')
     end
   end
 end
