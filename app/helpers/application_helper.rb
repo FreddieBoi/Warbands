21ffd6774e2d2330cd41e233a2f168b01ed15cec
Freddie <freddieboi@gmail.com>
2011-05-25 20:50:11 +0200

    Modified layout slightly and improved facebook like button (which is a helper method now)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,4 +25,10 @@ module ApplicationHelper
     link_to title, params.merge(:sort => column, :direction => direction, :page => nil), {:class => css_class}
   end
 
+  def facebook_like_button
+    src = "http://www.facebook.com/plugins/like.php?href=#{CGI::escape(request.url)}&layout=standard&show_faces=true&width=450&action=like&font=arial&colorscheme=light&height=80"
+    content_tag :iframe, nil, :src => src, :scrolling => 'no', :frameborder => '0',
+            :allowtransparency => true, :id => :facebook_like
+  end
+
 end
